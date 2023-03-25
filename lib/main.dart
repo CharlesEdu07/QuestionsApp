7a23e8d64b98f11448e053fdc2e5da6e403f63eb
Charles Eduardo <charleseduardofaria@gmail.com>
2023-03-25 14:29:55 -0300

    Usando column

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,33 @@ void main() => runApp(QuestionApp());
 class QuestionApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<String> questions = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('App de Perguntas'),
         ),
-        body: Text('Olá Flutter!'),
+        body: Column(
+          children: [
+            Text(questions.elementAt(0)),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            )
+          ],
+        ),
       )
     );
   }
